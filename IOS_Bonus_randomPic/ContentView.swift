ba557ee9cb6c9aaacb4ef4c49b3a6dd944106d54
Eric's MB <eric17618@gmail.com>
2020-12-16 16:32:56 +0800

    ver 1.0

--- IOS_Bonus_randomPic/ContentView.swift
+++ IOS_Bonus_randomPic/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            NetworkImage(urlStr: "https://loremflickr.com/240/480/cat")
+                .scaledToFit()
+            Image("cyberpunk")
+                .resizable()
+                .scaledToFit()
+                .offset(y: 300)
+        }
     }
 }
 
